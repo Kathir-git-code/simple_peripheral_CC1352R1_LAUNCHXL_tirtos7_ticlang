50f72f3e0ced697dcb5f9e534d3a3daa359e5b8d
LAVANYA11YA <lavanya.s@schnellenergy.com>
2025-07-28 13:13:19 +0530

    Initial commit

---
+++ Release/NPI/npi_tl.d
@@ -0,0 +1,265 @@
+# FIXED
+
+NPI/npi_tl.o: \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/npi_tl.c \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/xdc/std.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/Power.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/utils/List.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/DeviceFamily.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/power/PowerCC26XX.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/dpl/HwiP.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/dpl/ClockP.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/power/PowerCC26X2.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/Temperature.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/sys_ctrl.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_chip_def.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_memmap.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ints.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_sysctl.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_prcm.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_nvic.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aon_ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ddi_0_osc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_rfc_pwr.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_adi_3_refsys.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aon_pmctl.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aon_rtc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_fcfg1.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/interrupt.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/debug.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/cpu.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_cpu_scs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/rom.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/pwr_ctrl.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_adi_2_refsys.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/osc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ccfg.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ddi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/rom.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/ddi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aux_smph.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/prcm.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_memmap_common.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/aon_ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/adi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_uart.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_adi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/vims.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_vims.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/family/arm/m3/Hwi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/BIOS.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Types_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Error.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Error_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/family/arm/m3/Hwi_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/hal/Hwi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Task.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Queue.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Queue_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Clock.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Swi.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Swi_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Clock_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/IHeap.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Memory.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Memory_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Task_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/GPIO.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/gpio/GPIOCC26XX.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ioc.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/gpio.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_gpio.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_memmap.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ints.h \
+ syscfg/ti_drivers_config.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/icall/src/inc/icall.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/inc/hal_assert.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/target/_common/hal_types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/target/_common/_hal_types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/inc/hal_defs.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/inc/npi_tl.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/inc/npi_config.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/semaphore.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/sys/_internal.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/time.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/signal.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/sys/types.h \
+ C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/inc/npi_tl_uart.h
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/xdc/std.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/Power.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/utils/List.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/DeviceFamily.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/power/PowerCC26XX.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/dpl/HwiP.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/dpl/ClockP.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/power/PowerCC26X2.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/Temperature.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/sys_ctrl.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_chip_def.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_memmap.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ints.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_sysctl.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_prcm.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_nvic.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aon_ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ddi_0_osc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_rfc_pwr.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_adi_3_refsys.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aon_pmctl.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aon_rtc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_fcfg1.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/interrupt.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/debug.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/cpu.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_cpu_scs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/rom.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/pwr_ctrl.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_adi_2_refsys.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/osc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ccfg.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ddi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/rom.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/ddi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_aux_smph.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/prcm.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_memmap_common.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/aon_ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/adi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_uart.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_adi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/vims.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_vims.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/family/arm/m3/Hwi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/BIOS.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Types_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Error.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Error_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/family/arm/m3/Hwi_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/hal/Hwi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Task.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Queue.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Queue_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Clock.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Swi.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Swi_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Clock_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/IHeap.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Memory.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/runtime/Memory_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/kernel/tirtos7/packages/ti/sysbios/knl/Task_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/GPIO.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/drivers/gpio/GPIOCC26XX.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ioc.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/driverlib/gpio.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_gpio.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_memmap.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/devices/cc13x2_cc26x2/inc/hw_ints.h:
+
+syscfg/ti_drivers_config.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/icall/src/inc/icall.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/inc/hal_assert.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/target/_common/hal_types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/target/_common/_hal_types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/hal/src/inc/hal_defs.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/inc/npi_tl.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/inc/npi_config.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/semaphore.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/sys/_internal.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/time.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/signal.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/posix/ticlang/sys/types.h:
+
+C:/ti/simplelink_cc13xx_cc26xx_sdk_8_30_01_01/source/ti/ble5stack/npi/src/inc/npi_tl_uart.h:
